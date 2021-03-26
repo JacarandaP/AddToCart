cea799b452a23cd474715c4437826391b8fac705
Jacaranda Perez <jacaranda.perez@gmail.com>
2021-03-26 08:40:58 +0100

    Test again med Github

--- shop.js
+++ shop.js
@@ -47,7 +47,7 @@ $(document).ready(function(){
   }
   
   function load(){
-    let sourceApi = 'http://webacademy.se/fakestore/'
+    let sourceApi = 'https://webacademy.se/fakestore/'
     fetch(sourceApi).then(function(response){
      return response.json();}).then(function(json){
           showProduct(json);
@@ -249,124 +249,6 @@ $('#info').on("click", ".delete-prd",(function(event){
   $('#finalInfo').html(showFinalCart())
 
   
-/**
- * Validering av formulären
- */
- /*
-function test(){
-  validation = [];
 
-     let inputName = document.getElementById("clientsName");
-     if(isRequired(inputName.value) === false){
-       $('#input1').html("This field cannot be empty");
-     } else if (hasNumbers(inputName.value) === false){
-      $('#input1').html("No numbers allowed");
-      } else {
-       $('#input1').html("");
-     };
-     
-  let inputMail = document.getElementById("clientsMail");
-  if(isRequired(inputMail.value) === false){
-    $('#input3').html("This field cannot be empty")
-  } else if(isEmailValid(inputMail.value)== false){
-    $('#input3').html("Invalid email")
-  } else {
-    $('#input3').html("");
-  };
-
-  
-  let inputPhone = document.getElementById("clientsPhone");
-  if(isRequired(inputPhone.value) === false){
-    $('#input4').html("This field cannot be empty")
-  } else if (isPhoneValid(inputPhone.value) === false){
-    $('#input4').html("Not a valid phone number")
-  } else {
-    $('#input4').html("");
-  };
-  
-
-  let inputAddress = document.getElementById("clientsAddress");
-  if(isRequired(inputAddress.value) === false){
-    $('#input2').html("This field cannot be empty");
-  } else if(isAddressValid(inputAddress.value)=== false){
-    $('#input2').html("Just numbers and letters allowed")
-  } else {
-    $('#input2').html("");
-  }
-
-      function isRequired(input){
-        if(input == ""){
-          validation.push("false");
-          return false;
-        } else {
-          validation.push("true");
-          return true;
-        }
-      }
-  
-      function hasNumbers(input){
-        let characters= /^[a-zA-Z]*$/;
-        if(input.match(characters)){
-          validation.push("true");
-          return true
-        } else {
-          validation.push("false");
-          return false;
-        }
-      }
-  
-     function isEmailValid(email) {
-      let mailformat = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
-        if(email.match(mailformat)){
-          validation.push("true");
-          return true;
-        } else {
-          validation.push("false");
-          return false;
-        }
-      }
-  
-      function isPhoneValid (phonenr) {
-        let digits = /^\d{10}/;
-        if(phonenr.match(digits)) {
-          validation.push("true");
-          return true
-        } else {
-          validation.push("false");
-          return false;
-        }
-      }
-  
-  function isAddressValid(address){
-    let digitsAndNumbers = /^[a-zA-Z0-9- X]*$/;
-    if(address.match(digitsAndNumbers)){
-      validation.push("true");
-      return true
-    } else {
-      validation.push("false");
-      return false;
-    }
-  }
-  
-  let validationTest = validation.includes("false");
-
-      if(validationTest === false){
-        alert("your order has successfully been processed");
-        emptyCart()
-        $('#counter').html(getTotalAmountProducts());
-        
-      } else {
-       alert("Please fill in the form with correct values");
-      }
-  
-    }
-
-    let submitbtn = document.getElementById("confirmbtn")
-    submitbtn.addEventListener("click", function(event){
-       event.preventDefault()
-       test();
-    
-})
-   */
 })
 
